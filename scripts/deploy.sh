f8bd7dd1c7a4b016d75bf474e1ad22cf09d0c36f
Gabriel Esquivel <contact@gaboesquivel.com>
2016-02-10 18:17:33 -0600

    add automated deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+SOURCE_BRANCH="source"
+DESTINATION_BRANCH="master"
+
+echo "Starting deployment"
+echo "Source branch: $SOURCE_BRANCH | Target branch: $DESTINATION_BRANCH"
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+
+echo "Compiling site"
+git checkout -B $SOURCE_BRANCH
+grunt build || exit 1
+
+echo "Pushing new content to $ORIGIN_URL"
+cd www
+git config user.name "Travis-CI" || exit 1
+git config user.email "dev@castimirano.com" || exit 1
+
+git init
+git add -A . || exit 1
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" $DESTINATION_BRANCH > /dev/null 2>&1
+
+echo "Deployed successfully."
